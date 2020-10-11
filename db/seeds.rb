cec512730f6618ba4e3d885ac6a60b6b6b284f58
jadeson komolvipat <jadesonk@gmail.com>
2020-10-11 14:41:33 +0700

    test seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shop.create(name: 'Kusatsu Onsen', address: '97/15 Moo 5 Tambon Samed, Amphur Muang, Chonburi 20130', category: 'barbershop')
+
+Shop.create(name: 'MYTH Urban Massage', address: '28/3 Soi Ruamrudee, Wireless Road Lumpini, Pathumwan, Bangkok 10330', category: 'spa_and_massage')
+
+Shop.create(name: 'SIRI Massage & Spa', address: '20/2-3 Sukhumvit 22, Khlong Toei, Bangkok, Thailand 10110', category: 'nails')
+
+Shop.create(name: 'Chonthong Thai Massage & Spa', address: '55/29 Moo 10 Klong 1, Klong Luang, Pathum Thani 12120', category: 'hair_removal')
