9f68d998de6cfe46fc12ea89b0c344fcb6575a30
jadeson komolvipat <jadesonk@gmail.com>
2020-10-11 14:37:14 +0700

    add uploaded cloudinary icons for map marker for different shop categories

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,11 +2,20 @@ class ShopsController < ApplicationController
   def index
     @shops = Shop.where.not(latitude: nil, longitude: nil)
 
+    icons = {
+      spa_and_massage: 'https://res.cloudinary.com/dbwwrdzej/image/upload/v1602401313/map-challenge/spa_cx8ah3.png',
+      barbershop: 'https://res.cloudinary.com/dbwwrdzej/image/upload/v1602401313/map-challenge/hair-dryer_oipsvy.png',
+      nails: 'https://res.cloudinary.com/dbwwrdzej/image/upload/v1602401313/map-challenge/hand_u7utpt.png',
+      hair_removal: 'https://res.cloudinary.com/dbwwrdzej/image/upload/v1602401722/map-challenge/head-remove_yroqss.png',
+      not_set: nil,
+    }
+
     @markers = @shops.map do |shop|
       {
         lat: shop.latitude,
         lng: shop.longitude,
-        infoWindow: { content: render_to_string(partial: "/shops/map_box", locals: { shop: shop }) }
+        infoWindow: { content: render_to_string(partial: "/shops/map_box", locals: { shop: shop }) },
+        icon: icons[shop.category.to_sym]
       }
     end
   end
